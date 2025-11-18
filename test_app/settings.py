64767c195e13e52f979bc35e50b490df0fa83c3d
YASSINE SAIQ <158505591+Yassinesaiq@users.noreply.github.com>
2025-11-18 13:49:31 +0100

    Update settings.py

--- test_app/settings.py
+++ test_app/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 
 ]
 
@@ -139,6 +140,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]  # dossier où tu mets tes fichiers CSS, JS, images "statics"
