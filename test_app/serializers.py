6a4fb4dea14900f7584f2b95aa4b3c104fec8046
YASSINE SAIQ <yassine.saiq@usmba.ac.ma>
2025-11-21 12:02:32 +0100

    score lsit correction

--- test_app/serializers.py
+++ test_app/serializers.py
@@ -38,6 +38,7 @@ class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topics
         fields = ['topic_id','topic','description', 'image_url','topic_type']
+        
     def get_image_url(self, obj):
         if obj.configuration_img:
             return self.context['request'].build_absolute_uri(obj.configuration_img.url)
