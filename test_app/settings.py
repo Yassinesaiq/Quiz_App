2d5b26d0a802ac41a76202cf0865a9537d164cc9
YASSINE SAIQ <158505591+Yassinesaiq@users.noreply.github.com>
2026-01-28 15:15:17 +0100

    Update settings.py

--- test_app/settings.py
+++ test_app/settings.py
@@ -30,8 +30,9 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '10.0.2.2',
     '192.168.26.77:38640',
-    'mb-quiz-xshw.onrender.com',
+    ' mbquiz-e9o7p.ondigitalocean.app',
     'hammerhead-app-eaa9e.ondigitalocean.app'
+   
 ]
 
 
