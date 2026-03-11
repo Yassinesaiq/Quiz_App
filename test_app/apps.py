d723400c8ca6f49400a7e4b98ba95a30a5a477a1
saiqy <yassine.saiq@usmba.ac.ma>
2026-03-11 10:07:20 +0100

    Update apps.py

--- test_app/apps.py
+++ test_app/apps.py
@@ -5,11 +5,3 @@ from datetime import timedelta
 class AppConfig(AppConfig):
     name = 'test_app'
 
-    def ready(self):
-        from test_app.models import QuizResult, TestSession, TextAnswer
-        threshold = timezone.now() - timedelta(days=60)
-        old_sessions = TestSession.objects.filter(started_at__lt=threshold)
-        for session in old_sessions:
-            TextAnswer.objects.filter(session=session).delete()
-            QuizResult.objects.filter(session=session).delete()
-            session.delete()
